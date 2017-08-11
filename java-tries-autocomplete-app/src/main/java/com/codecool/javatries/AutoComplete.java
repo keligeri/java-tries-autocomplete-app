f7279ea5b9a8cb2b1de70fefe3a3da103794cbad
keligeri <kel.gergo@gmail.com>
2017-08-11 15:45:42 +0200

    Implement addWord method with some basic examples

--- java-tries-autocomplete-app/src/main/java/com/codecool/javatries/AutoComplete.java
+++ java-tries-autocomplete-app/src/main/java/com/codecool/javatries/AutoComplete.java
@@ -1,5 +1,7 @@
 package com.codecool.javatries;
 
+import sun.text.normalizer.Trie;
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -20,8 +22,20 @@ public class AutoComplete {
      * @param wordToAdd
      */
     public void addWord(String wordToAdd) {
-
+        TrieDataNode current = root;
+        for (char currentChar : wordToAdd.toCharArray()) {
+            TrieDataNode child = isChildContainChar(current, currentChar);
+            if (child == null) {
+                TrieDataNode newNode = new TrieDataNode(currentChar);
+                current.addChild(newNode);
+                current = newNode;
+            } else {
+                current = child;
+            }
+        }
+        current.setTerminate(true);
     }
+
 
     /**
      * Returns the possible completions of baseChars String from the Trie.
@@ -30,6 +44,23 @@ public class AutoComplete {
      */
     public List<String> autoComplete(String baseChars) {
         List<String> words = new ArrayList<>();
+//        TrieDataNode current = root;
+//
+//        for (int i = 0; i < wordToAdd.length(); i++){
+//            TrieDataNode child = isChildContainChar(current, wordToAdd.charAt(i));
+//            if (child != null){
+//                current = child;
+//            }
+//        }
+        return null;
+    }
+
+    private TrieDataNode isChildContainChar(TrieDataNode node, char searchedChar){
+        for (TrieDataNode child : node.getChild()) {
+            if (child.getData() == searchedChar){
+                return child;
+            }
+        }
         return null;
     }
 
@@ -40,5 +71,17 @@ public class AutoComplete {
      */
     public boolean removeWord(String wordToRemove) {
         return false;
+    }
+
+    public static void main(String[] args) {
+        AutoComplete ac = new AutoComplete();
+        ac.addWord("fun");
+        ac.addWord("funky");
+        ac.addWord("fund");
+        ac.addWord("fundament");
+        ac.addWord("fundamental");
+        ac.addWord("a");
+        ac.addWord("abc");
+        ac.addWord("acb");
     }
 }
--- java-tries-autocomplete-app/src/main/java/com/codecool/javatries/TrieDataNode.java
+++ java-tries-autocomplete-app/src/main/java/com/codecool/javatries/TrieDataNode.java
@@ -1,11 +1,15 @@
 package com.codecool.javatries;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class TrieDataNode {
 
     private char data;
+    private List<TrieDataNode> child = new ArrayList<>();
+    private boolean isTerminate;
+
 
     /**
      * Initializes a TrieDataNode with its data
@@ -21,6 +25,22 @@ public class TrieDataNode {
      */
     public char getData() {
         return data;
+    }
+
+    public void addChild(TrieDataNode child){
+        this.child.add(child);
+    }
+
+    public boolean getIsTerminate() {
+        return isTerminate;
+    }
+
+    public void setTerminate(boolean terminate) {
+        isTerminate = terminate;
+    }
+
+    public List<TrieDataNode> getChild() {
+        return child;
     }
 
     @Override
